72244d82c9170d724c63b39c4125b1f87823ad12
Atuls-Cloud <atuls7718@gmail.com>
2025-03-26 00:04:04 +0530

    commit

--- src/main/java/com/email/writer/EmailWriterSbApplication.java
+++ src/main/java/com/email/writer/EmailWriterSbApplication.java
@@ -2,8 +2,10 @@ package com.email.writer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.email.writer.app")//new
 public class EmailWriterSbApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/email/writer/app/EmailGeneratorController.java
+++ src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -1,17 +1,21 @@
 package com.email.writer.app;
 
-import lombok.AllArgsConstructor;
+//import lombok.AllArgsConstructor;............new
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 //"api/email/generate"
 @RestController
 @RequestMapping("api/email")
-@AllArgsConstructor
+//@AllArgsConstructor......new
 @CrossOrigin(origins = "*")
 public class EmailGeneratorController {
     private final EmailGeneratorService emailGeneratorService;
-
+//new
+    public EmailGeneratorController(EmailGeneratorService emailGeneratorService) {
+        this.emailGeneratorService = emailGeneratorService;
+    }
+//new close
     @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
         String response = emailGeneratorService.generateEmailReply(emailRequest);
